c76faf2b57e718e392af814e880cba8dd33d8043
MamadovZaki <mamadovzicx@gmail.com>
2022-01-15 22:38:32 +0200

    Updates quotes_spider.py: Updated parse function to be able to export data as json or jl

--- Tutorial/QuotesScraper/QuotesScraper/spiders/quotes_spider.py
+++ Tutorial/QuotesScraper/QuotesScraper/spiders/quotes_spider.py
@@ -2,29 +2,21 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     """
-    name: identifies the Spider. It must be unique within a project, that is, you can’t set the same name for different Spiders.
+    start_urls is a class attribute with a list of URLs
 
-    start_requests(): must return an iterable of Requests (you can return a list of requests or write a generator function) which the Spider will begin to crawl from. Subsequent requests will be generated successively from these initial requests.
-
-    parse(): a method that will be called to handle the response downloaded for each of the requests made. The response parameter is an instance of TextResponse that holds the page content and has further helpful methods to handle it.
-
-    The parse() method usually parses the response, extracting the scraped data as dicts and also finding new URLs to follow and creating new requests (Request) from them.
-
+    start_urls list will then be used by the default implementation of start_requests() to create the initial requests for your spider
     """
     name = 'quotes'
 
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+    'http://quotes.toscrape.com/page/1/'
+    ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'saved file {filename}')
+        quotes = response.xpath("//div[@class='quote']")
+        for quote in quotes:
+            yield {
+                'text' : quote.css('span.text::text').get(),
+                'author' : quote.css('small.author::text').get(),
+                'tags' : quote.css('div.tags a.tag::text').getall()
+            }
