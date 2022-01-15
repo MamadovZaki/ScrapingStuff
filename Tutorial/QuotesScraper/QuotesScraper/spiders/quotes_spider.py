fe2ed51a987890c89f118b18bbef6f725a8f3b86
MamadovZaki <mamadovzicx@gmail.com>
2022-01-15 22:46:30 +0200

    Updates quotes_spider.py: Now our spider is able to follow links

--- Tutorial/QuotesScraper/QuotesScraper/spiders/quotes_spider.py
+++ Tutorial/QuotesScraper/QuotesScraper/spiders/quotes_spider.py
@@ -20,3 +20,7 @@ class QuotesSpider(scrapy.Spider):
                 'author' : quote.css('small.author::text').get(),
                 'tags' : quote.css('div.tags a.tag::text').getall()
             }
+        next_page =response.xpath("//li[@class='next']/a/@href").get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
